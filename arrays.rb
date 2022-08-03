3ca5b395ececa7d739e5b6500216169036334cac
Amanda Ross <amikaross@gmail.com>
2022-08-03 09:36:10 -0600

    Call methods on the arrays

--- arrays.rb
+++ arrays.rb
@@ -2,3 +2,11 @@ friends = ["Lee", "Becca", "Erin", "Jared"]
 numbers = [1, 2, 3, 4]
 floats = [1.1, 2.1, 3.1, 4.1]
 booleans = [true, false, true, false]
+#This will remove "Jared" from the friends array and return "Jared"
+friends.pop
+#This will add the number 5 to the end of the numbers array and return the updated array
+numbers.push(5)
+#This will remove the float 1.1 from the floats array and return 1.1
+floats.shift
+#This will insert the boolean true to the beginning of the booleans array  and return the updated array
+boolean.unshift(true)
